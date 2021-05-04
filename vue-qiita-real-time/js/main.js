3380a67f634237dc0e9838575727141d93fa32a6
Kento.Yamada <masterhinagiku@gmail.com>
2021-05-04 10:43:06 +0900

    Udemy VueJs contents

---
+++ vue-qiita-real-time/js/main.js
@@ -0,0 +1,44 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        items: null,
+        keyword: '',
+        message:''
+    },
+    watch: {
+        keyword: function (newkeyword, oldkeyword) {
+            // console.log(newkeyword)
+            this.message = 'waiting for you stop typing...'
+            this.debouncedGetAnswer()
+        }
+    },
+    created: function () {
+        // this.keyword = 'JavaScript'
+        // this.getAnswer()
+        this.debouncedGetAnswer = _.debounce(this.getAnswer,1000)
+    },
+    methods: {
+        getAnswer: function () {
+            if (this.keyword === '') {
+                this.items = null
+                this.message = ''
+                return 
+            }
+
+            this.message = 'Loading...'
+            var vm = this
+            var params = { page: 1, perpage: 20, query: this.keyword }
+            axios.get('https://qiita.com/api/v2/items', { params })
+                .then(function (response) {
+                    console.log(response)
+                    vm.items=response.data
+                })
+                .catch(function (error) {
+                    vm.message = 'Error!' + error          
+                })
+                .finally(function () {
+                    vm.message=''
+            })
+        }
+    }
+})
